65d62002659e3534f3c43cd77e97b8bf55690ee1
nerun <danieldiasr@gmail.com>
2025-07-03 15:28:14 -0300

    portuguese (pt) translation

---
+++ lang/pt/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ */
+$lang['error']                = "ERRO:";
+$lang['no_such_function']     = "a função não existe!";
+$lang['not_enough_params']    = "não foram fornecidos parâmetros suficientes!";
+$lang['empty_test_parameter'] = "o parâmetro de teste não pode estar vazio!";
+$lang['invalid_syntax']       = "sintaxe inválida!";
+$lang['invalid_expression']   = 'expressão contém caracteres inválidos.';
+$lang['undefined_result']     = 'resultado indefinido ou não numérico.';
+$lang['evaluation_error']     = 'erro ao avaliar a expressão.';
+
